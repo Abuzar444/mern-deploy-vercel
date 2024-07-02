9b093cca544f7b5c3e9bc35bedf50aabca6f0b96
= <=>
2024-07-02 12:58:53 +0500

    commit

--- server/server.js
+++ server/server.js
@@ -29,27 +29,27 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-// if (process.env.NODE_ENV === 'development') {
-//   app.use(morgan('dev'));
-// }
-// app.use(express.static(path.resolve(__dirname, './client/dist')));
-// app.use(cookieParser());
-// app.use(express.json());
-// app.use(helmet());
-// app.use(mongoSanitize());
+const __dirname = dirname(fileURLToPath(import.meta.url));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.static(path.resolve(__dirname, './client/dist')));
+app.use(cookieParser());
+app.use(express.json());
+app.use(helmet());
+app.use(mongoSanitize());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-// app.get('/api/v1/test', (req, res) => {
-//   res.json({ msg: 'test route' });
-// });
+app.get('/api/v1/test', (req, res) => {
+  res.json({ msg: 'test route' });
+});
 
-// app.use('/api/v1/jobs', authenticateUser, jobRouter);
-// app.use('/api/v1/users', authenticateUser, userRouter);
-// app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobRouter);
+app.use('/api/v1/users', authenticateUser, userRouter);
+app.use('/api/v1/auth', authRouter);
 
 app.get("*", (req, res) => {
   res.status(200).json({ message: "Hello world" })
@@ -61,13 +61,13 @@ app.get('*', (req, res) => {
 
 // why it don't work
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+});
 
-// app.use('*', (req, res) => {
-//   res.status(404).json({ msg: 'not found' });
-// });
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'not found' });
+});
 
 app.use(errorHandlerMiddleware);
 
